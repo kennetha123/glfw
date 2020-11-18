ee29d7a0cb7c6cccd70b18686e310ebcfec4e8aa
kennetha123 <jodysag5@gmail.com>
2020-11-18 08:07:46 +0700

    - fix premake5 compilation.

--- premake5.lua
+++ premake5.lua
@@ -2,8 +2,8 @@ project "GLFW"
   kind "StaticLib"
   language "C"
   
-  targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-  objdir ("obj/" ..outputdir .. "/%{prj.name}")
+  targetdir ("bin/" .. outputDir .. "/%{prj.name}")
+  objdir ("obj/" ..outputDir .. "/%{prj.name}")
   
   files
   {
@@ -25,20 +25,20 @@ project "GLFW"
     
       files
       {
-        "src/win32_init.c"
-        "src/win32_joystick.c"
-        "src/win32_monitor.c"
-        "src/win32_time.c"
-        "src/win32_thread.c"
-        "src/win32_window.c"
-        "src/wgl_context.c"
-        "src/egl_context.c"
+        "src/win32_init.c",
+        "src/win32_joystick.c",
+        "src/win32_monitor.c",
+        "src/win32_time.c",
+        "src/win32_thread.c",
+        "src/win32_window.c",
+        "src/wgl_context.c",
+        "src/egl_context.c",
         "src/osmesa_context.c"
       }
       
       defines
       {
-        "_GLFW_WIN32"
+        "_GLFW_WIN32",
         "_CRT_SECURE_NO_WARNINGS"
       }
       filter { "system:windows", "configurations:Release" }
