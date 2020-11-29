1d9f59695947abf07d2e853ece631fee1432756f
kennetha123 <jodysag5@gmail.com>
2020-11-29 15:06:25 +0700

    - fix premake5.lua to static

--- premake5.lua
+++ premake5.lua
@@ -1,7 +1,8 @@
 project "GLFW"
   kind "StaticLib"
   language "C"
-  
+  staticruntime "on"
+
   targetdir ("bin/" .. outputDir .. "/%{prj.name}")
   objdir ("obj/" ..outputDir .. "/%{prj.name}")
   
@@ -19,9 +20,7 @@ project "GLFW"
   }
   
   filter "system:windows"
-    buildoptions { "-std=c11", "-lgdi32" }
     systemversion "latest"
-    staticruntime "On"
     
       files
       {
@@ -41,5 +40,12 @@ project "GLFW"
         "_GLFW_WIN32",
         "_CRT_SECURE_NO_WARNINGS"
       }
-      filter { "system:windows", "configurations:Release" }
-        buildoptions "/MT"
+	  
+	  -- filter configuration of VS
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+	
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
